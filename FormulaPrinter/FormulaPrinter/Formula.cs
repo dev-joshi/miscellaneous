cb63c9cbd1e45fbdbefdd7f418eab4f97a9c5383
Devansh Joshi <devansh.joshi96@gmail.com>
2021-12-25 18:42:54 +0530

    Formula Printer
    cli tool to visualize 2 varible formula in table with varying x and y

---
+++ FormulaPrinter/FormulaPrinter/Formula.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Text;
+
+namespace FormulaPrinter
+{
+    public class Formula
+    {
+        private Func<float, float, float> formula;
+
+        public Formula(Func<float, float, float> formula)
+        {
+            this.formula = formula;
+        }
+
+        public string Print(float startX, float incX, int iX, float startY, float incY, int iY)
+        {
+            var sb = new StringBuilder();
+
+            float x = startX;
+            float y = startY;
+
+            int i = 0;
+            int j = 0;
+
+            sb.Append("X\\Y     |");
+            for (j = 0; j < iY; j++)
+            {
+                sb.Append(string.Format($"{y:00.000}") + "  ");
+                y += incY;
+            }
+
+            sb.Append("\n\n");
+            y = startY;
+
+            for (i = 0; i < iX; i++)
+            {
+                sb.Append(string.Format($"{x:00.000}") + "  |");
+
+                for (j = 0; j < iY; j++)
+                {
+                    sb.Append(Calculate(x, y) + "  ");
+                    y += incY;
+                }
+
+                sb.Append("\n");
+                y = startY;
+                x += incX;
+            }
+
+            return sb.ToString();
+        }
+
+        private string Calculate(float x, float y)
+        {
+            string ans = "ERR";
+
+            try
+            {
+                ans = string.Format($"{formula.Invoke(x, y):00.000}");
+            }
+            catch (Exception)
+            {
+                // Ignore math errors.
+            }
+
+            return ans;
+        }
+    }
+}
---
+++ FormulaPrinter/FormulaPrinter/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace FormulaPrinter
+{
+    class Program
+    {
+        private static float result(float x, float y)
+        {
+            // Edit this formula...
+            return (y * 5f) / (x + y);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to 2 variable formula printer!");
+
+            var f = new Formula(result);
+
+            float startX, incX, startY, incY;
+            int iX, iY;
+
+            if (args.Length < 6)
+            {
+                if(!GetInput("starting point of X", out startX)
+                    || !GetInput("increment in X", out incX)
+                    || !GetInput("iterations of X", out iX)
+                    || !GetInput("starting point of Y", out startY)
+                    || !GetInput("increment in Y", out incY)
+                    || !GetInput("iterations of Y", out iY))
+                {
+                    return;
+                }
+            }
+            else
+            {
+                if(!ValidateInput(args[0], out startX)
+                    || !ValidateInput(args[1], out incX)
+                    || !ValidateInput(args[2], out iX)
+                    || !ValidateInput(args[3], out startY)
+                    || !ValidateInput(args[4], out incY)
+                    || !ValidateInput(args[5], out iY))
+                {
+                    return;
+                }
+            }
+
+            Console.WriteLine(f.Print(startX, incX, iX, startY, incY, iY));
+            Console.ReadKey();
+        }
+
+        private static bool GetInput<T>(string name, out T val) where T : struct
+        {
+            Console.WriteLine($"Please enter the {name} :");
+            var input = Console.ReadLine();
+            return ValidateInput(input, out val);
+        }
+
+        private static bool ValidateInput<T>(string input, out T val) where T : struct
+        {
+            val = default(T);
+            try
+            {
+                var converter = TypeDescriptor.GetConverter(typeof(T));
+
+                if (converter is not null)
+                {
+                    val = (T)converter.ConvertFromString(input);
+                    return true;
+                }
+            }
+            catch (Exception)
+            {
+            }
+
+            Console.WriteLine("Incorrect input!!!!");
+
+            return false;
+        }
+    }
+}
